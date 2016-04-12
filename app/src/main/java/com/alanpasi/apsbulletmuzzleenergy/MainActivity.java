cc5d8df72a5f3173f3db50d20676144767ce85be
Alvaro Paz da Silva <alanpasi@gmail.com>
2016-04-12 12:38:35 -0300

    Update MainActivity.java

--- app/src/main/java/com/alanpasi/apsbulletmuzzleenergy/MainActivity.java
+++ app/src/main/java/com/alanpasi/apsbulletmuzzleenergy/MainActivity.java
@@ -9,5 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        TextView textViewLabelBullet = (TextView) findViewById(R.id.textViewLblBulletWeight);
+        textViewLabelBullet.setVisibility(View.INVISIBLE);
+        
     }
 }
