5de3ea607e7cbcba260cce656e168225c65a8601
alanpasi <alanpasi@gmail.com>
2016-05-07 16:52:05 -0300

    New input mode.

--- app/src/main/java/com/alanpasi/apsbulletmuzzleenergy/MainActivity.java
+++ app/src/main/java/com/alanpasi/apsbulletmuzzleenergy/MainActivity.java
@@ -2,14 +2,18 @@ package com.alanpasi.apsbulletmuzzleenergy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.text.DecimalFormat;
 
 
-public class MainActivity extends AppCompatActivity implements OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     EditText etBulletWeight;
     EditText etBulletVelocity;
@@ -25,27 +29,48 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
         TextView energyResult = (TextView) findViewById(R.id.textViewLblResult);
 
-        energyResult.setOnClickListener(this);
-
         etBulletWeight = (EditText) findViewById(R.id.editTextBulletWeight);
         etBulletVelocity = (EditText) findViewById(R.id.editTextBulletVelocity);
         etBulletEnergy = (EditText) findViewById(R.id.editTextResult);
+
+        etBulletWeight.addTextChangedListener(watch);
+        etBulletVelocity.addTextChangedListener(watch);
     }
 
-// Method when tap on Muzzle Energy text
+// Method when input text
 // https://en.wikipedia.org/wiki/Muzzle_energy
-    @Override
-    public void onClick(View v) {
 
 
-        if (etBulletWeight.getText().toString().length() > 0 && etBulletVelocity.getText().toString().length() > 0) {
-            double bulletWeight = Double.parseDouble(etBulletWeight.getText().toString());
-            double bulletVelocity = Double.parseDouble(etBulletVelocity.getText().toString());
-            double bulletEnergy = ((1d/2d) * (bulletWeight * (bulletVelocity * bulletVelocity))) * FOOT_POUND_FORCE;
+    TextWatcher watch = new TextWatcher(){
 
-            DecimalFormat decimalFormat = new DecimalFormat("#.##");
-            etBulletEnergy.setText(decimalFormat.format(bulletEnergy));
+        @Override
+        public void afterTextChanged(Editable arg0) {
+            // TODO Auto-generated method stub
+
         }
 
-    }
+        @Override
+        public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int a, int b, int c) {
+            // TODO Auto-generated method stub
+            if (etBulletWeight.getText().toString().length() > 0 && etBulletVelocity.getText().toString().length() > 0) {
+                double bulletWeight = Double.parseDouble(etBulletWeight.getText().toString());
+                double bulletVelocity = Double.parseDouble(etBulletVelocity.getText().toString());
+                double bulletEnergy = ((1d/2d) * (bulletWeight * (bulletVelocity * bulletVelocity))) * FOOT_POUND_FORCE;
+
+                DecimalFormat decimalFormat = new DecimalFormat("#.##");
+                etBulletEnergy.setText(decimalFormat.format(bulletEnergy));
+            }
+
+//            output.setText(s);
+//            Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+            if(a == 9){
+                Toast.makeText(getApplicationContext(), "Maximum Limit Reached", Toast.LENGTH_SHORT).show();
+            }
+        }};
 }
